66e9699e01252acb1e86f0441e0315f3a8042174
gabrielleon31 <gabrielestudio31@gmail.com>
2024-10-16 20:05:26 +0200

    Subida practica 16/10

--- M3/UF1/AC2/MP03BUF1AC2_GabrielLeon/obj/Debug/net8.0/MP03BUF1AC2_GabrielLeon.AssemblyInfo.cs
+++ M3/UF1/AC2/MP03BUF1AC2_GabrielLeon/obj/Debug/net8.0/MP03BUF1AC2_GabrielLeon.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("MP3BAC1_Gabriel_Leon")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("MP03BUF1AC2_GabrielLeon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31ca1ac561cd544694c6adc36e12d4914643f915")]
-[assembly: System.Reflection.AssemblyProductAttribute("MP3BAC1_Gabriel_Leon")]
-[assembly: System.Reflection.AssemblyTitleAttribute("MP3BAC1_Gabriel_Leon")]
+[assembly: System.Reflection.AssemblyProductAttribute("MP03BUF1AC2_GabrielLeon")]
+[assembly: System.Reflection.AssemblyTitleAttribute("MP03BUF1AC2_GabrielLeon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
