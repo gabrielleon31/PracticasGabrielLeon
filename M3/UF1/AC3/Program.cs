80762623739a53b9f08294fcabe191ee346a001b
gabrielleon31 <bygama31@gmail.com>
2025-01-09 00:55:08 +0100

    Restauracion

---
+++ M3/UF1/AC3/Program.cs
@@ -0,0 +1,99 @@
+﻿namespace NavesEstelares
+{
+    class NaveEstelar
+    {
+        public string Nombre { get; set; }
+        public int VelocidadMaxima { get; set; }
+
+        public NaveEstelar(string nombre, int velocidadMaxima)
+        {
+            Nombre = nombre;
+            VelocidadMaxima = velocidadMaxima;
+        }
+
+        public virtual void ActivarNave()
+        {
+            Console.WriteLine($"La {Nombre} ha activado sus sistemas.");
+        }
+
+        public virtual void EjecutarMision()
+        {
+            Console.WriteLine($"La {Nombre} está realizando una misión de exploración.");
+        }
+
+        public void ApagarNave()
+        {
+            Console.WriteLine($"La {Nombre} ha apagado sus sistemas.");
+        }
+    }
+
+    class NaveDeCombate : NaveEstelar
+    {
+        public int PotenciaAtaque { get; set; }
+
+        public NaveDeCombate(string nombre, int velocidadMaxima, int potenciaAtaque)
+            : base(nombre, velocidadMaxima)
+        {
+            PotenciaAtaque = potenciaAtaque;
+        }
+
+        public override void ActivarNave()
+        {
+            Console.WriteLine($"La {Nombre} ha activado sus sistemas de combate.");
+        }
+
+        public override void EjecutarMision()
+        {
+            Console.WriteLine($"La {Nombre} está atacando con potencia de fuego nivel {PotenciaAtaque}.");
+        }
+    }
+
+    class NaveDeCarga : NaveDeCombate
+    {
+        public int CapacidadCarga { get; set; }
+
+        public NaveDeCarga(string nombre, int velocidadMaxima, int potenciaAtaque, int capacidadCarga)
+            : base(nombre, velocidadMaxima, potenciaAtaque)
+        {
+            CapacidadCarga = capacidadCarga;
+        }
+
+        public override void ActivarNave()
+        {
+            Console.WriteLine($"La {Nombre} ha activado sus sistemas.");
+        }
+
+        public override void EjecutarMision()
+        {
+            Console.WriteLine($"La {Nombre} está atacando con potencia de fuego nivel {PotenciaAtaque}.");
+            Console.WriteLine($"La {Nombre} está defendiendo el espacio alrededor del planeta X.");
+        }
+
+        public void MostrarCarga()
+        {
+            Console.WriteLine($"La nave de carga lleva una cantidad de carga de {CapacidadCarga} Kg.");
+        }
+    }
+
+    internal class Programa
+    {
+        static void Main()
+        {
+            NaveEstelar nave1 = new NaveEstelar("nave estelar básica", 500);
+            nave1.ActivarNave();
+            nave1.EjecutarMision();
+            nave1.ApagarNave();
+
+            NaveDeCombate nave2 = new NaveDeCombate("nave de combate", 800, 7);
+            nave2.ActivarNave();
+            nave2.EjecutarMision();
+            nave2.ApagarNave();
+
+            NaveDeCarga nave3 = new NaveDeCarga("nave de carga", 400, 10, 2500);
+            nave3.ActivarNave();
+            nave3.EjecutarMision();
+            nave3.MostrarCarga();
+            nave3.ApagarNave();
+        }
+    }
+}
---
+++ M3/UF1/ExamenEstudio/Program.cs
@@ -0,0 +1,151 @@
+﻿// class Program {   
+// private static void Main(string[] args) {
+// Usuario usuario = new Usuario();
+// UsuarioVIP usuarioVIP = new UsuarioVIP { Nombre = "Ana", NivelVIP = 2 };
+// usuario.MensajeConsola();  
+// usuarioVIP.MensajeConsola(); }}
+
+// public class Usuario {
+// public string Nombre { get; set; }
+// public virtual void MensajeConsola () {
+// Console.WriteLine("Holaaaa");}}
+
+// public class UsuarioVIP : Usuario {
+// public int NivelVIP { get; set; }
+// public override void MensajeConsola(){
+// Console.WriteLine($"El usuario {Nombre} tiene un nivel de {NivelVIP}");}}
+
+
+//-------------------------------------------------------------------------------------
+
+// public class Libro {
+// public string Titulo {get; set; }
+// public string Autor {get; set;}
+// public Libro(string titulo, string autor) {
+// Titulo = titulo;
+// Autor = autor; }
+
+// public void MostrarDatos () {
+// Console.WriteLine($"Titulo: {Titulo}, Autor: {Autor}");}}
+
+// class Program {
+// public static void Main(string[] args) {
+// Libro milibro = new Libro("LemamaLema", "Lema");
+// milibro.MostrarDatos();}}
+
+//------------------------------------------------------------------------------------
+
+// public class Persona {
+
+//     public string Nombre { get; set;}
+
+//     public int Edad { get; set;}
+
+//     //tendria que crear un constructor
+
+//     public Persona (string nombre, int edad) {
+
+//         Nombre = nombre;
+
+//         Edad = edad;
+//     }
+// }
+
+// class Program {
+
+//     public static void Main(string[] args) {
+
+//     Persona persona1 = new Persona("Lema", 30);
+
+//     Console.WriteLine($"Nombre: {persona1.Nombre}, Edad: {persona1.Edad}");
+//     }
+// }
+
+//------------------------------------------------------------------------------------------
+
+// public class Persona
+
+// {
+//     protected string nombrePersona;   
+//     public Persona(string nombre)
+//     {
+//         nombrePersona = nombre;
+//     }
+
+//     public void GetNombre(){
+
+//         Console.WriteLine("el nombre de la persona es " + nombrePersona);
+
+//     }
+// }
+
+// public class Jefe : Persona
+// {
+//     public string Departamento { get; set; }
+
+//     public Jefe(string nombre, string departamento) : base(nombre)
+//     {
+//         Departamento = departamento;
+//     }
+
+//     public void GetDepartament() {
+
+//         Console.WriteLine("el departamente es " + Departamento);
+//     }
+// }
+
+// public class Empleado : Persona
+// {
+//     public string Salario { get; set; }
+
+//     public Empleado(string nombre, string salario) : base(nombre)
+//     {
+//         Salario = salario;
+//     }
+
+//     public void GetSalario() {
+
+//         Console.WriteLine("el departamente es " + Salario);
+//     }
+// }
+
+// class Program {
+
+//     static void Main(string[] args) {
+
+//         Jefe jefe = new Jefe("Alice", "Desarrollo");
+//         Empleado empleado = new Empleado("Bob", "50000");
+//         Persona persona1 = new Persona("Lema");
+//         // jefe.GetNombre(); 
+//         // empleado.GetNombre(); 
+//         // jefe.GetDepartament();
+//         // empleado.GetSalario();
+//         persona1.GetNombre();
+//     }
+// }
+
+//-----------------------------
+
+// class A  
+// {  
+//     public virtual void show()  
+//     {  
+//         Console.WriteLine("Hello: Base Class!");  
+//         Console.ReadLine();  
+//     }  
+// }  
+  
+
+// class B : A  
+// {  
+//     public override void show()  
+//     {  
+//         Console.WriteLine("Hola!");  
+//         Console.ReadLine();  
+//     }  
+// }
+
+//-------------------------------------------------------------------------
+
+
+
